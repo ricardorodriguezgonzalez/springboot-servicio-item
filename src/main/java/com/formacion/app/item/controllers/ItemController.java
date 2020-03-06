3bedbe699f7c83e30e899b672229d53b2b505834
Ricardo Rodríguez <rrodrigueg.seiza@bancochile.cl>
2020-03-06 11:15:41 -0300

    Configurando Hystrix para el manejo de errorres y tolerancia a fallos

--- src/main/java/com/formacion/app/item/controllers/ItemController.java
+++ src/main/java/com/formacion/app/item/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.formacion.app.item.controllers;
 
 import com.formacion.app.item.models.Item;
+import com.formacion.app.item.models.Producto;
 import com.formacion.app.item.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +23,22 @@ public class ItemController {
     public List<Item> listar(){
         return itemService.findAll();
     }
+
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
         return itemService.findById(id, cantidad);
     }
+
+    public Item metodoAlternativo(Long id, Integer cantidad){
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Ricardo");
+        producto.setPrecio(5000.00);
+        item.setProducto(producto);
+        return item;
+    }
+
 }
